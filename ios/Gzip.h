54881cc7a41e9a97088b51ded66cbd4b3592a667
cixio <5869764+cixio@users.noreply.github.com>
2025-03-29 22:44:35 +0100

    changes for new arch

--- ios/Gzip.h
+++ ios/Gzip.h
@@ -1,12 +1,5 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNGzipSpec.h"
-
-@interface Gzip : NSObject <NativeGzipSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface Gzip : NSObject <RCTBridgeModule>
-#endif
 
 @end
--- ios/Gzip.mm
+++ ios/Gzip.mm
@@ -24,13 +24,4 @@ RCT_REMAP_METHOD(deflate,
   resolve([[_data gzippedData] base64String]);
 }
 
-// Don't compile this code when we build for the old architecture.
-#ifdef RCT_NEW_ARCH_ENABLED
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeGzipSpecJSI>(params);
-}
-#endif
-
 @end
